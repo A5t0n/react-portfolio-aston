b0491745ce1bfc5fe3f423ad6e2c86dfd0b5d1bf
Aston Varghese <astonvarghese8@gmail.com>
2024-12-01 17:47:53 -0500

    added socials into the navbar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faUser,faHome,faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -12,6 +13,7 @@ const Sidebar = () => (
         <img className="sub-logo" src={LogoSubtitle} alt="slobodan"/>
         </Link>
         <nav>
+
             <NavLink exact="true" activeclassname="active" to="/">
             <FontAwesomeIcon icon={faHome} color=" #4d4d4e" />
             </NavLink>
@@ -19,11 +21,28 @@ const Sidebar = () => (
             <NavLink exact="true" activeclassname="active"  className="about-link" to="/about">
             <FontAwesomeIcon icon={faUser} color=" #4d4d4e" />
             </NavLink>
-            
+
             <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
             <FontAwesomeIcon icon={faEnvelope} color=" #4d4d4e" />
             </NavLink>
+
         </nav>
+
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/a5t0n08/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/A5t0n'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+
+        </ul>
+
     </div>
 )
 
