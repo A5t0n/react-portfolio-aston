423901c8e2838a0fec46165edb64b211ae5430dc
Aston Varghese <astonvarghese8@gmail.com>
2024-12-19 12:45:10 -0500

    created an animation for hover over the other letters of intro.  The error that pops up during useeffect can be cleared using clearTimeout function

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,23 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['l','o','b','o','d','a','n']
     const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r']
+
+    useEffect (()=> {
+        const timeoutId= setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, []);
 
     return(
         <div className="container home-page">
