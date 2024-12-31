7a938e9c0d6e392122a9398381e909bba514383a
Aston Varghese <astonvarghese8@gmail.com>
2024-12-31 12:45:13 -0500

    created a portfolio.json file and rendered the image in portfolio.json

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,58 @@
 import React from "react";
+import "./index.scss";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
+import portfolioData from "../../data/portfolio.json"
 
 const Portfolio = () => {
-    return <div>Hello</div>;
-};
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+        console.log(portfolioData);
+
+
+        useEffect (()=> {
+                const timeoutId= setTimeout(() => {
+                    setLetterClass('text-animate-hover')
+                }, 3000);
+        
+                return () => {
+                    clearTimeout(timeoutId);
+                };
+            }, []);
+
+            const renderPortfolio = (portfolio) => {
+                return(
+                    <div className="images-container">
+                        {
+                            portfolio.map((port,idx) => {
+                               return(
+                                <div className="image-box" key={idx}>
+                                    <img className="portfolio-image" src={port.cover} alt="portfolio"/>
+                                </div>
+                               ) 
+                            })
+                        }
+                    </div>
+                );
+            }
+
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={"Portfolio".split("")} 
+                        idx={15}   
+                    />
+                </h1>
+                <div>{renderPortfolio(portfolioData.portfolio)}</div>
+            </div>
+            <Loader type="pacman" />
+        </>
+        
+    )
+}
 
 export default Portfolio;
